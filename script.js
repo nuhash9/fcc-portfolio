cf860c122502316edc3275713d6d82c1fdabbeda
Nuhash Chowdhury <nuhashchowdhury@hotmail.com>
2016-06-15 16:36:04 +0600

    finished the portfolio section and updated the about section

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-//myscript
-$(function() {
+$(function(){
   $('nav ul li a').bind('click', function(){
     $(this).parent().addClass('active').siblings().removeClass('active');
   });
+  smoothScroll(500);
+  //$('#content').scrollspy({target: '#nav' });
 });
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
