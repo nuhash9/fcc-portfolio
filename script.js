268b8a84b70e87ea655b2c29f77f205e31ecdcf7
Nuhash Chowdhury <nuhashchowdhury@hotmail.com>
2016-06-16 13:37:32 +0600

    input proper nav scroll functionality using materialize.css

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
 $(function(){
-  $('nav ul li a').bind('click', function(){
-    $(this).parent().addClass('active').siblings().removeClass('active');
-  });
-  smoothScroll(500);
-  //$('#content').scrollspy({target: '#nav' });
+  $('.scrollspy').scrollSpy();
 });
-
-function smoothScroll (duration) {
-	$('a[href^="#"]').on('click', function(event) {
-
-	    var target = $( $(this).attr('href') );
-
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
-	});
-}
